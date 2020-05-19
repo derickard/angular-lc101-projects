8140def5e177178beaff86637372bab91416a3c0
unknown <derickard@gmail.com>
2020-05-19 09:59:49 -0400

    practice completed

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-   movies = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
-
+   movies: string[] = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
+   movieExists: boolean = false;
+   movieInvalid: boolean = false;
    constructor() { }
 
    ngOnInit() {
+   }
+
+   addMovie( newTitle: string) {
+      if(!this.movies.includes(newTitle) && newTitle !== '') {
+         this.movies.push(newTitle);
+      } else if (this.movies.includes(newTitle)) { 
+         this.movieExists = true;
+      } else if (newTitle === '') {
+         this.movieInvalid = true;
+      }
    }
 }
